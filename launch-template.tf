06acacc517d52cec187365f0a4a119353934bfde
nitin-das <nitin.das@thomson.co.uk>
2020-01-13 13:59:41 +0000

    Modified Code

---
+++ launch-template.tf
@@ -0,0 +1,56 @@
+resource "aws_launch_template" "launch_template" {
+  name_prefix   = var.name_prefix
+  description   = var.description
+  image_id      = var.image_id
+  ebs_optimized = var.ebs_optimized
+  dynamic "block_device_mappings" {
+    for_each = var.block_device_mappings
+    content {
+      device_name  = lookup(block_device_mappings.value, "device_name", null)
+      no_device    = lookup(block_device_mappings.value, "no_device", null)
+      virtual_name = lookup(block_device_mappings.value, "virtual_name", null)
+
+      dynamic "ebs" {
+        for_each = lookup(block_device_mappings.value, "ebs", [])
+        content {
+          delete_on_termination = lookup(ebs.value, "delete_on_termination", null)
+          encrypted             = lookup(ebs.value, "encrypted", null)
+          iops                  = lookup(ebs.value, "iops", null)
+          kms_key_id            = lookup(ebs.value, "kms_key_id", null)
+          snapshot_id           = lookup(ebs.value, "snapshot_id", null)
+          volume_size           = lookup(ebs.value, "volume_size", null)
+          volume_type           = lookup(ebs.value, "volume_type", null)
+        }
+      }
+    }
+  }
+
+  dynamic "iam_instance_profile" {
+    for_each = [var.iam_instance_profile]
+    content {
+      arn  = lookup(iam_instance_profile.value, "arn", null)
+      name = lookup(iam_instance_profile.value, "name", null)
+    }
+  }
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+  dynamic "monitoring" {
+    for_each = var.monitoring
+    content {
+      enabled = lookup(monitoring.value, "enabled", null)
+    }
+  }
+
+  dynamic "tag_specifications" {
+    for_each = var.tag_specifications
+    content {
+      resource_type = lookup(tag_specifications.value, "resource_type", null)
+      tags          = lookup(tag_specifications.value, "tags", null)
+    }
+  }
+
+  user_data = var.user_data
+  tags      = var.common_tags
+  key_name  = var.key_name
+}
+
---
+++ variables.tf
@@ -0,0 +1,56 @@
+## Launch Template
+variable "name_prefix" {
+  description = "Creates a unique name beginning with the specified prefix."
+}
+
+variable "description" {
+  description = "Description of the launch template."
+}
+
+variable "image_id" {
+  description = "The AMI from which to launch the instance."
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized."
+}
+
+variable "block_device_mappings" {
+  description = "Specify volumes to attach to the instance besides the volumes specified by the AMI."
+  type        = list(string)
+}
+
+variable "iam_instance_profile" {
+  description = "The IAM Instance Profile to launch the instance with."
+  type        = list(string)
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of security group IDs to associate with."
+  type        = list(string)
+}
+
+variable "monitoring" {
+  description = "The monitoring option for the instance. See Monitoring below for more details."
+  type        = list(string)
+}
+
+variable "user_data" {
+  description = "The Base64-encoded user data to provide when launching the instance."
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance."
+}
+
+variable "tag_specifications" {
+  description = "The tags to apply to the resources during launch."
+  type        = list(string)
+}
+
+## Tags
+variable "tags" {
+  description = "Resources Tags"
+  type        = map(string)
+}
+
